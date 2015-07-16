6e344a7b4046d537551e2f0aedb6ef38a3b46b2a
shahar halutz <shahar.halutz@gmail.com>
2015-07-16 20:12:01 +0200

    cleanup

--- popup/popup.js
+++ popup/popup.js
@@ -12,28 +12,12 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse) {
 });
 
 
-function loginComplete(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-
-        chrome.tabs.sendMessage(tabs[0].id, {
-            "gesture": 'openAddOrganization',
-            "options": {}
-        }, function(response) {
-            console.log(response.farewell);
-        });
-        
-    });	
-}
-
-
-
 // slack login :
 $(document).on('click', 'button#OAuth', function() {
     
     console.log('OAuth dance initiated');
 	chrome.runtime.sendMessage({message: 'beginOAuth'});
   
-	//loginComplete();
 });
 
 $(document).on('click', '#logout', function() {
