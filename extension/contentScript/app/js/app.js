0808bc07e6ea0c7750786cbe81dc1dc2c341fda7
Adam Goldman <adamgoldman7@gmail.com>
2015-10-02 14:57:15 +0200

    temp point API to refactor

--- extension/contentScript/app/js/app.js
+++ extension/contentScript/app/js/app.js
@@ -2,9 +2,9 @@
 
 var myApp = angular.module('MyApp', ['ui.slider', 'ngResource', 'ngTouch', 'ngMessages', 'ui.router', 'ui.bootstrap', 'satellizer','BFAPIServices','ngSanitize','ui.select']);
 
-myApp.constant('environmentURL', 'https://stagingenviornment.elasticbeanstalk.com/');
+myApp.constant('environmentURL', 'https://refactor.elasticbeanstalk.com/');
 myApp.constant('SLACK_API_URL', 'https://slack.com/api/');
-myApp.constant('API_URL', 'https://stagingenviornment.elasticbeanstalk.com/');
+myApp.constant('API_URL', 'https://refactor.elasticbeanstalk.com/');
 
 myApp.config(function($stateProvider, $urlRouterProvider, $authProvider, $compileProvider) {
     $stateProvider
