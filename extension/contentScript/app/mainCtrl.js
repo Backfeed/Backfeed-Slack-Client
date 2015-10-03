19412ff909dfde978794037d7be641283a28acb3
Adam Goldman <adamgoldman7@gmail.com>
2015-10-04 00:52:06 +0200

    refactor mainCtrl

--- extension/contentScript/app/mainCtrl.js
+++ extension/contentScript/app/mainCtrl.js
@@ -1,99 +1,143 @@
-angular.module('MyApp').controller('MainCtrl', ["$scope", "$state","PostMessageService",
-	function($scope, $state, PostMessageService) {
-	
-	//************    Listen to incoming messages in order to open modal and navigate the App       ************
-	function goToAddProject(channelId) {
-		console.log('Creating Project');
-		//PostMessageService.navigateToAddProject();
-		$state.go('addProject', {'channelId': channelId}, {reload: true});
-	}
-	
-	function goToProjectStatus(channelId) {
-		console.log('Project Status');
-		$state.go('projectStatus', {'channelId': channelId,'mileStoneId': ''}, {reload: true});
-	}
-	
-	function goToMileStoneStatus(mileStoneId) {
-		console.log('Project Status from MileStone');
-		$state.go('projectStatus', {'channelId': '','mileStoneId': mileStoneId}, {reload: true});
-	}
+angular.module('MyApp')
+	.controller('MainCtrl', MainCtrl);
 
-	function goToAddMilestone(channelId) {
-		console.log('Creating Milestone');
-		//PostMessageService.navigateToAddMilestone(); // NOT IMPLEMENTED
-		$state.go('addMilestone', {'channelId': channelId}, {reload: true});
-	}
 
-	function goToAddContribution(channelId) {
-		console.log('Starting contributions'+channelId);
-		$state.go('createContribution', {'channelId': channelId}, {reload: true});
-	}
-	
-	function goToAddEvaluation(contributionId) {
-		console.log('Starting evaluations for contributionID: '+contributionId);
-		$state.go('evaluations', {'contributionId': contributionId}, {reload: true});
-	}
-	
-	function goToAddMileStoneEvaluation(mileStoneId) {
-		console.log('Starting evaluations for mileStoneId: '+mileStoneId);
-		$state.go('mileStoneEvaluations', {'mileStoneId': mileStoneId}, {reload: true});
-	}
-	
-	function goToContributionStatus(contributionId) {
-		console.log('Starting showing contribution status: '+contributionId);
-		$state.go('contributionStatus', {'contributionId': contributionId,'mileStoneId': ''}, {reload: true});
-	}
+function MainCtrl($state, _DEV, PostMessageService) {
 
-	function goToMemberStatus(memberId) {
-		console.log('Starting showing member status: '+memberId);
-		$state.go('memberStatus', {'memberId': memberId}, {reload: true});
-	}
-	
-	function showAlert() {
-		console.log('Showing Alert');
-		PostMessageService.showAlert('You need to login in order to use the extension. Click the extension icon above.', 'error');
-	}
-	
-	function refreshWindows() {
-		console.log('Refresh Windows');
-		PostMessageService.showAlert('Successful authorized. Please continue', 'success');
-		PostMessageService.windowRefresh();
-	}
-	
-	function logout() {
-		console.log('Logging Out');
-		PostMessageService.windowRefresh();
-	}
+  var log = _DEV.log("MAIN CTRL");
 
-	var GESTURES = {
-		"openAddProject": goToAddProject,
-		"openAddMilestone": goToAddMilestone,
-		"openAddContributionPage": goToAddContribution,
-		"openAddEvaluationPage": goToAddEvaluation,
-		"openAddMileStoneEvaluationPage": goToAddMileStoneEvaluation,
-		"showAlertFromMainCtr": showAlert,
-		"openContributionStatusPage": goToContributionStatus,
-		"openMemberStatusPage": goToMemberStatus,
-		"refreshWindows": refreshWindows,
-		"openProjectStatus":goToProjectStatus,
-		"openMileStoneStatus":goToMileStoneStatus,
-		"logout": logout
-	};
+  var GESTURES = {
 
-	console.log('MainCtrl init');
+    "openAddProject": goToAddProject,
+    "openAddMilestone": goToAddMilestone,
+    "openAddContributionPage": goToAddContribution,
+    "openAddEvaluationPage": goToAddEvaluation,
+    "openAddMileStoneEvaluationPage": goToAddMileStoneEvaluation,
+    "showAlertFromMainCtr": showAlert,
+    "openContributionStatusPage": goToContributionStatus,
+    "openMemberStatusPage": goToMemberStatus,
+    "refreshWindows": refreshWindows,
+    "openProjectStatus": goToProjectStatus,
+    "openMileStoneStatus": goToMileStoneStatus,
+    "logout": logout
 
-	var init = function() {
-		PostMessageService.init("myport");
+  };
 
-		console.log('MainCtrl: init: listening to events. ');
+  init();
 
-		chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-			console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-			if(request.gesture && request.gesture in GESTURES) {
-				GESTURES[request.gesture](request.options)
-		}
-		});
-	};
-	init();
-}]);
+  function init() {
 
+    log('init');
+    PostMessageService.init("myport");
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+      if (request.gesture && request.gesture in GESTURES) {
+        GESTURES[request.gesture](request.options)
+      }
+    });
+  }
+
+  //************    Listen to incoming messages in order to open modal and navigate the App       ************
+  function goToAddProject(channelId) {
+    log('Creating Project');
+    $state.go('addProject', {
+      'channelId': channelId
+    }, {
+      reload: true
+    });
+  }
+
+  function goToProjectStatus(channelId) {
+    log('Project Status');
+    $state.go('projectStatus', {
+      'channelId': channelId,
+      'mileStoneId': ''
+    }, {
+      reload: true
+    });
+  }
+
+  function goToMileStoneStatus(mileStoneId) {
+    log('Project Status from MileStone');
+    $state.go('projectStatus', {
+      'channelId': '',
+      'mileStoneId': mileStoneId
+    }, {
+      reload: true
+    });
+  }
+
+  function goToAddMilestone(channelId) {
+    log('Creating Milestone');
+    //PostMessageService.navigateToAddMilestone(); // NOT IMPLEMENTED
+    $state.go('addMilestone', {
+      'channelId': channelId
+    }, {
+      reload: true
+    });
+  }
+
+  function goToAddContribution(channelId) {
+    log('Starting contributions' + channelId);
+    $state.go('createContribution', {
+      'channelId': channelId
+    }, {
+      reload: true
+    });
+  }
+
+  function goToAddEvaluation(contributionId) {
+    log('Starting evaluations for contributionID: ' + contributionId);
+    $state.go('evaluations', {
+      'contributionId': contributionId
+    }, {
+      reload: true
+    });
+  }
+
+  function goToAddMileStoneEvaluation(mileStoneId) {
+    log('Starting evaluations for mileStoneId: ' + mileStoneId);
+    $state.go('mileStoneEvaluations', {
+      'mileStoneId': mileStoneId
+    }, {
+      reload: true
+    });
+  }
+
+  function goToContributionStatus(contributionId) {
+    log('Starting showing contribution status: ' + contributionId);
+    $state.go('contributionStatus', {
+      'contributionId': contributionId,
+      'mileStoneId': ''
+    }, {
+      reload: true
+    });
+  }
+
+  function goToMemberStatus(memberId) {
+    log('Starting showing member status: ' + memberId);
+    $state.go('memberStatus', {
+      'memberId': memberId
+    }, {
+      reload: true
+    });
+  }
+
+  function showAlert() {
+    log('Showing Alert');
+    PostMessageService.showAlert('You need to login in order to use the extension. Click the extension icon above.', 'error');
+  }
+
+  function refreshWindows() {
+    log('Refresh Windows');
+    PostMessageService.showAlert('Successful authorized. Please continue', 'success');
+    PostMessageService.windowRefresh();
+  }
+
+  function logout() {
+    log('Logging Out');
+    PostMessageService.windowRefresh();
+  }
+
+}
