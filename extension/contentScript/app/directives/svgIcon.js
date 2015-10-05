ece0edc8f9a7c33e0274bcd0a6b43f1bdb880af8
Adam Goldman <adamgoldman7@gmail.com>
2015-10-06 01:54:35 +0200

    refactor svgIcon directive

--- extension/contentScript/app/directives/svgIcon.js
+++ extension/contentScript/app/directives/svgIcon.js
@@ -1,11 +1,26 @@
-angular.module('MyApp').directive('svgIcon', function() {
-    return {
-        restrict: 'E',
-        template: '<span ng-include="getIconUrl()"></span>',
-        link: function (scope, element, attrs) {
-            scope.getIconUrl = function() {
-                return chrome.extension.getURL("extension/contentScript/app/images/svg/" + attrs.icon + ".html");
-            }
-        }
-    };
-});
+angular.module('MyApp').directive('svgIcon', svgIcon);
+
+
+function svgIcon)() {
+
+  return {
+
+    restrict: 'E',
+    template: '<span ng-include="getIconUrl()"></span>',
+    link: link
+    
+  };
+
+  function link(scope, element, attrs) {
+
+    scope.getIconUrl = getIconUrl;
+    
+    function getIconUrl() {
+
+      return chrome.extension.getURL("extension/contentScript/app/images/svg/" + attrs.icon + ".html");
+
+    }
+
+  }
+
+}
