081c44387e11d716712b45d363a383a56fa094ec
Adam Goldman <adamgoldman7@gmail.com>
2015-10-08 16:06:59 +0200

    refactor contributionsTable with controllerAs

--- extension/contentScript/app/directives/contributionsTable/contributionsTable.js
+++ extension/contentScript/app/directives/contributionsTable/contributionsTable.js
@@ -1,10 +1,12 @@
 angular.module('MyApp')
-.directive('contributionsTable', contributionsTable);
+  .directive('contributionsTable', contributionsTable);
 
 function contributionsTable() {
 
   return {
 
+    bindToController: true,
+    controllerAs: 'ctrl',
     templateUrl: chrome.extension.getURL("extension/contentScript/app/directives/contributionsTable/contributionsTable.html"),
     controller: contributionsTableController,
     scope: { contributions: '=', code: '=' },
@@ -15,5 +17,5 @@ function contributionsTable() {
 }
 
 function contributionsTableController($scope) {
-  console.log(' ************** hello from contributions table! **************');
+
 }
