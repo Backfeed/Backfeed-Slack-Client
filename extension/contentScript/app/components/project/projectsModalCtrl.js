98d24cf5d5614e9854a883f49e3e3e31270b8de4
Adam Goldman <adamgoldman7@gmail.com>
2015-10-11 12:41:32 +0200

    Creat eproejct fix

--- extension/contentScript/app/components/project/projectsModalCtrl.js
+++ extension/contentScript/app/components/project/projectsModalCtrl.js
@@ -1,7 +1,7 @@
 angular.module('MyApp')
   .controller('ProjectsModalCtrl', ProjectsModalCtrl);
 
-function ProjectsModalCtrl($stateParams, _DEV, Slack, $modalInstance, PostMessageService, CurrentUser) {
+function ProjectsModalCtrl($stateParams, _DEV, Slack, $modalInstance, PostMessageService, CurrentUser, Project) {
 
   log = _DEV.log('NEW PROJECT');
 
