27f30d2aa6ae2eb45f1c525c3c13c40f0aa8f603
Adam Goldman <adamgoldman7@gmail.com>
2015-10-13 23:26:39 +0200

    add stripWhitespace directive

---
+++ extension/contentScript/app/directives/stripWhitespace.js
@@ -0,0 +1,28 @@
+angular.module('MyApp')
+  .directive('stripWhitespace', stripWhitespace);
+
+function stripWhitespace(_DEV) {
+
+  var log = _DEV.log("STRIP WHITEPSACE");
+
+  return {
+
+    require: 'ngModel',
+    link:  stripWhitespaceLink
+
+
+  };
+
+  function stripWhitespaceLink(scope, element, attrs, modelCtrl) {
+
+    element.on('keydown', function(e) {
+      if ( e.keyCode === 32 ) {
+        log("no spacebar plz!");
+        e.preventDefault();
+        return false;
+      }
+    });
+
+  }
+
+}
