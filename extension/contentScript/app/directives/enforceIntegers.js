ebf81dca72f38d385e99e21da357a4b654d5a62b
Adam Goldman <adamgoldman7@gmail.com>
2015-10-13 23:54:24 +0200

    add minimum 0 check to enforce integers

--- extension/contentScript/app/directives/enforceIntegers.js
+++ extension/contentScript/app/directives/enforceIntegers.js
@@ -16,20 +16,29 @@ function enforceIntegers(_DEV) {
   function enforceIntegersLink(scope, element, attrs, modelCtrl) {
 
     element.on('keydown', function(e) {
-      log("modelCtrl", modelCtrl);
+
       if ( e.keyCode === 69 || e.keyCode === 189 || e.keyCode === 190 ) {
         log("'e' / '.' / '-'  detected, censored! only integers plz!");
         e.preventDefault();
         return false;
       }
+
     });
 
     element.on('keyup', function(e) {
+
       if ( modelCtrl.$$rawModelValue > 100 ) {
         log("Oh ha! looks like you are way beyond your limit! (100)");
         modelCtrl.$setViewValue(100);
         modelCtrl.$render();
       }
+
+      if ( e.keyCode === 48 && modelCtrl.$error.min && modelCtrl.$$rawModelValue === 0 ) {
+        log("Integer be greater than 0!");
+        modelCtrl.$setViewValue(null);
+        modelCtrl.$render();
+      }
+
     });
 
   }
