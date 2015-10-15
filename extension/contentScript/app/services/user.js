acbfe6ff7f055f73bbc5b7d8173f391ac038e16c
Adam Goldman <adamgoldman7@gmail.com>
2015-10-15 21:36:22 +0200

    add resource service

---
+++ extension/contentScript/app/services/user.js
@@ -0,0 +1,30 @@
+angular.module('MyApp')
+  .service('User', User);
+
+function User(_DEV, Resource) {
+
+  var log = _DEV.log('USER SERVICE');
+
+  var service = {
+
+    get: get,
+    getProjects: getProjects,
+    getProject: getProject
+
+  };
+
+  return service;
+
+  function get(userId) {
+    return Resource.get('api/user', { userId: userId });
+  }
+
+  function getProjects(userId) {
+    return Resource.get('member/status/' + userId);
+  }
+
+  function getProject(userId, projectId) {
+    return Resource.get('member/status/' + userId + '/' + projectId);
+  }
+
+}
