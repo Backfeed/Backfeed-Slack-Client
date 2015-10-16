c656242e84889663268e54b829bbb28e23bf6051
Adam Goldman <adamgoldman7@gmail.com>
2015-10-16 14:10:35 +0200

    refactor resource calls base params
    access_token -> slackAccessToken, userId and slackTeamId removed
    
    access_token is renamed to be more descriptive, userId and slackTeamId aren't needed since server gets satelizer_token from header and can idetify the user based on that

--- extension/contentScript/app/services/resource.js
+++ extension/contentScript/app/services/resource.js
@@ -51,16 +51,10 @@ function Resource($http, API_URL, _DEV, $localStorage) {
   }
 
   function getBaseParams() {
-    var access_token = $localStorage.currentUser ? $localStorage.currentUser.access_token : null;
-    var slackTeamId = $localStorage.currentUser ? $localStorage.currentUser.slackTeamId : null;
-    var userId = $localStorage.currentUser ? $localStorage.currentUser.userId : null;
+    var slackAccessToken = $localStorage.currentUser ? $localStorage.currentUser.slackAccessToken : null;
 
     return { 
-
-      access_token: access_token,
-      slackTeamId: slackTeamId,
-      userId: userId
-
+      slackAccessToken: slackAccessToken 
     };
   }
   
