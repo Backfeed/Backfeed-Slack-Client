75a017876e417f56105c1b3d29cd756f92767e9f
Adam Goldman <adamgoldman7@gmail.com>
2015-10-21 00:05:58 +0200

    Contribution service-  add evaluate method

--- extension/contentScript/app/services/contribution.js
+++ extension/contentScript/app/services/contribution.js
@@ -8,7 +8,8 @@ function Contribution(_DEV, Resource, Slack) {
   var service = {
 
     create: create,
-    get: get
+    get: get,
+    evaluate: evaluate
 
   };
 
@@ -26,25 +27,30 @@ function Contribution(_DEV, Resource, Slack) {
 
   }
   
-  	function buildContributionMessage(contributionToSubmit,contributionId) {
-	    var contributorsString = '';
-	    var contributorsLength = contributionToSubmit.contributors.length;
-	    var index = 0;
-	    contributionToSubmit.contributors.forEach(function(contributor) {
-	      if (index == contributorsLength - 1) {
-	        contributorsString += '@' + contributor.name + ' ' + contributor.percentage + '%';
-	      } else {
-	        contributorsString += '@' + contributor.name + ' ' + contributor.percentage + '%, ';
-	      }
-	      index++;
-	    });
-
-	    return 'New contribution submitted' + '\n' + contributionId + '\n' + '*' + contributionToSubmit.title + '*' + '\n' + contributionToSubmit.description + '\n' + contributorsString;
-	  };
-
   function get(contributionId) {
     return Resource.get('contribution/' + contributionId);
   }
+
+  function evaluate(contributionId, evaluation) {
+    return Resource.post('bids', contributionId, evaluation);
+  }
+
+  function buildContributionMessage(contributionToSubmit,contributionId) {
+    var contributorsString = '';
+    var contributorsLength = contributionToSubmit.contributors.length;
+    var index = 0;
+    contributionToSubmit.contributors.forEach(function(contributor) {
+      if (index == contributorsLength - 1) {
+        contributorsString += '@' + contributor.name + ' ' + contributor.percentage + '%';
+      } else {
+        contributorsString += '@' + contributor.name + ' ' + contributor.percentage + '%, ';
+      }
+      index++;
+    });
+
+    return 'New contribution submitted' + '\n' + contributionId + '\n' + '*' + contributionToSubmit.title + '*' + '\n' + contributionToSubmit.description + '\n' + contributorsString;
+  }
+
 
   function getPreparedContributionForCreation(contribution) {
 
